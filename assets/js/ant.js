823b3fb7bfd86db4425719f8fd8955a306f84f63
Anthony <harkin1987@gmail.com>
2022-10-10 13:18:57 +0100

    Added site files

---
+++ assets/js/ant.js
@@ -0,0 +1,11 @@
+$('.scroll-to-smooth').on('click',function(e) {
+	e.preventDefault();
+	var offset = 0;
+	var target = this.hash;
+	if ($(this).data('offset') != undefined) offset = $(this).data('offset');
+	$('html, body').stop().animate({
+		'scrollTop': $(target).offset().top - offset
+	}, 500, 'swing', function() {
+		// window.location.hash = target;
+	});
+});
